80c6143d299d1b62174e9671d63ac7b1dddc25cc
magmacoffee <magmacoffees@gmail.com>
2023-11-22 08:34:30 +0900

    feat: datasaver가 실제로 wisepaas 저장하도록 수정

--- job/datasaver.py
+++ job/datasaver.py
@@ -3,29 +3,20 @@ datasaver
 =============
 Data Notifier로 부터 전달받은 센서 데이터를 Datahub에 저장하는 역할 수행
 """
-import threading
-import time
 from util.logger import log
-from wisepaas.wisepaas import WisePaas
 
 class SensorDataSaver:
-  def __init__(self, config, notifier):
+  def __init__(self, client, config, notifier):
+    self.__client = client
+    self.__client.connect()
+
     self.__config = config
     self.__notifier = notifier
     notifier.register(self)
     
+    
   def notify(self, value):
-    print(f"Data Saver: {value}")
-
-  def __job(self):
-      log("Job start")
-
-      while True:
-        # 여기에 코드 작성할 것!!!!
-        time.sleep(1)
-
-  def start(self):
-    threading.Thread(target=self.__job).start()
+    self.__client.save_edge_data(value)
 
   def __del__(self):
     self.__notifier.unregister(self)
