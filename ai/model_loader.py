e7ec1e187de2dcbacb8c4b223dcfc0c6bb9a25ff
magmacoffee <magmacoffees@gmail.com>
2023-11-23 23:40:55 +0900

    fix: powermgmt model_loader 로드 시점 및 방식 변경

--- ai/model_loader.py
+++ ai/model_loader.py
@@ -1,14 +1,28 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-
+from os.path import isfile
+from util.logger import log
 
 class model_loader:
     def __init__(self):
-        self.model = keras.models.load_model('./my_model')
+        self.__model = None
+
+    def load_model(self):
+        if not self.__model == None:
+            return True
+        if not isfile('./my_model'):
+            return False
+        try:
+            self.__model = keras.models.load_model('./my_model')
+            return True
+        except:
+            return False
 
     def predict(self, new_input_flow):
+        if self.__model == None:
+            raise Exception("Please load model first.")
         new_input_flow = new_input_flow.reshape((1, 10, 1))
-        predicted_output_flow = self.model.predict(new_input_flow)
+        predicted_output_flow = self.__model.predict(new_input_flow)
         return predicted_output_flow[0][0]
 
--- job/powermgmt.py
+++ job/powermgmt.py
@@ -9,19 +9,14 @@ import ai.model_loader as model_loader
 class PowerManagement:
   def __init__(self, notifier):
      self.__notifier = notifier
+     self.__model = model_loader.model_loader()
      notifier.register(self)
 
   def notify(self, value):
-    print(f"Power Mgmt: {value}")
-    # 모델 객체 생성
-    m = model_loader.model_loader()
     # 임의로 만들어줌 나중에 매개변수 넣을 수 있으면 지워야함.
     input_list = []
-    while True:
-      # 여기에 코드 작성할 것!!!!
-      # 모델 예측을 위해 인풋 리스트 필요함
-      result = m.predict(input_list)
-      return result
+    if self.__model.load_model():
+      self.__model.predict(input_list)
 
   def __del__(self):
      self.__notifier.unregister(self)   
