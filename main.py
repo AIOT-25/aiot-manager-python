e2fe49272dabf1c9e022133935689a88efc40903
magmacoffee <magmacoffees@gmail.com>
2023-11-23 23:41:51 +0900

    feat: SerialListener 추가 및 Event flag 주입

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from util.logger import Logger
 from job.datanotifier import SensorDataNotifier
 from job.powermgmt import PowerManagement
 from job.datasaver import SensorDataSaver
+from job.seriallistener import SerialListener
 from config import ManagerConfig
 
 c = ManagerConfig()
@@ -15,13 +16,21 @@ flag_init_sensor_thread = threading.Event()
 flag_thread_stop = threading.Event()
 
 if __name__ == '__main__':
-  Logger.create_logger()
+  try:
+    Logger.create_logger()
 
-  modbusClient = ModbusClientFactory.get_client(DUMMY, c.get_modbus_config())
-  wisepaasClient = WisePaasClient(c.get_wisepaas_config())
+    modbusClient = ModbusClientFactory.get_client(DUMMY, c.get_modbus_config())
+    wisepaasClient = WisePaasClient(c.get_wisepaas_config())
 
-  notifier = SensorDataNotifier(modbusClient)
-  notifier.start()
+    notifier = SensorDataNotifier(modbusClient)
+    notifier.start(flag_thread_stop)
 
-  datasaver = SensorDataSaver(wisepaasClient, c, notifier)
-  powermgmt = PowerManagement(notifier)
+    datasaver = SensorDataSaver(wisepaasClient, c, notifier)
+    powermgmt = PowerManagement(notifier)
+
+    seriallistener = SerialListener()
+    seriallistener.start()
+  except Exception as ex:
+    flag_thread_stop.set()
+    print("프로그램 실행 중 오류가 발생하였습니다.")
+    exit(0)
